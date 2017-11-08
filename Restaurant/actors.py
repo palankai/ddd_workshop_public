3a29832eb449d4d2fa1ad21b375635d4c745acb2
Gil Gonçalves <lursty@gmail.com>
2017-11-08 15:45:52 +0000

    Added Cook

--- Restaurant/actors.py
+++ Restaurant/actors.py
@@ -10,6 +10,15 @@ class Waiter:
         order = Order()
         self.handler.handle(order)
 
+class Cook:
+
+    def __init__(self, handler):
+        self.handler = handler
+
+    def handle(self, order):
+        order.add_line({'item': 'Cheese', 'qty': 1, 'price': 1})
+        self.handler.handle(order)
+
 
 class OrderPrinter:
 
--- Restaurant/test_actors.py
+++ Restaurant/test_actors.py
@@ -3,6 +3,10 @@ import actors
 
 class TestWaiter:
 
-    def test_placing_order(self):
+    def test_waiter_placing_order(self):
         waiter = actors.Waiter(actors.OrderPrinter())
+        waiter.place_order()
+
+    def test_cook_and_waiter_placing_order(self):
+        waiter = actors.Waiter(actors.Cook(actors.OrderPrinter()))
         waiter.place_order()
